19b9b4780c41490ff1c7af1d33be1407846c3f2c
SRB77 <soumyach144@gmail.com>
2025-01-18 14:04:57 +0530

    Added the Reactrouter into project to handle routing and navigation to different pages and testd by adding different files .

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+    <h1>here is a about page </h1>
+    </>
+  )
+}
+
+export default About
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable no-unused-vars */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import About from './components/About'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Createtrip from './create-trip/Createtrip'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App/>
+  },
+  {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"/create-trip",
+    element:<Createtrip/>
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
