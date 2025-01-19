05d175033007758d3c18df2621c4b27b3b464c0c
SRB77 <soumyach144@gmail.com>
2025-01-19 21:55:28 +0530

    Added input field from shadcn and the constants file for Datafile and and bit UI changes in create-trip page .

--- src/create-trip/Createtrip.jsx
+++ src/create-trip/Createtrip.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable no-unused-vars */
-import { Import } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { Input } from "@/components/ui/input";
 
 const Createtrip = () => {
+  const [place, setPlace] = useState();
   return (
     <>
       <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
@@ -14,15 +15,31 @@ const Createtrip = () => {
           Just provide some basic information about your travel preferences, and
           we&apos;ll create a personalized itinerary for you.
         </p>
-        <div className="mt-20">
+        <div className="mt-20 flex flex-col gap-10">
           <div className="">
-            <h2 className="text-xl my-3 font-medium">
+            <h2 className="text-xl mt-3 font-medium">
               What is destination of choice?
             </h2>
             <GooglePlacesAutocomplete
               apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+              selectProps={{
+                value: place,
+                onChange: (v) => {
+                  setPlace(v);
+                  console.log(v);
+                },
+              }}
             />
           </div>
+        </div>
+        <div>
+          <h2 className="text-xl mt-16 font-medium ">
+            How many days are you planning your trip?
+          </h2>
+          <Input placeholder={"Ex.3"} type="number" />
+        </div>
+        <div>
+          <h2 className="text-xl mt-16 font-medium ">What is Your Budget?</h2>
         </div>
       </div>
     </>
